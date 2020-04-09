ae3b2d41823eca64cd62c95f2584fc973f3c57c4
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-04-09 00:03:15 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -40,4 +40,10 @@ class MusicLibraryController
       end
     end
   end
+
+  def list_songs
+    Song.all.sort {|a,b| a.name <=> b.name}.each.with_index(1) do |song, i|
+      puts "#{i}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+    end
+    
 end
